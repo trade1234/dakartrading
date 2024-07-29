fa4669d032f19fd242ed56fcc05055d6cfef5fff
testnati <testnati95@gmail.com>
2024-07-29 17:25:58 +0300

    first

---
+++ assets/js/main2.js
@@ -0,0 +1,42 @@
+(function() {
+    "use strict";
+    /**
+     * Auto generate the carousel indicators
+     */
+    document.querySelectorAll('.carousel-indicators').forEach((carouselIndicator) => {
+      carouselIndicator.closest('.carousel').querySelectorAll('.carousel-item').forEach((carouselItem, index) => {
+        if (index === 0) {
+          carouselIndicator.innerHTML += `<li data-bs-target="#${carouselIndicator.closest('.carousel').id}" data-bs-slide-to="${index}" class="active"></li>`;
+        } else {
+          carouselIndicator.innerHTML += `<li data-bs-target="#${carouselIndicator.closest('.carousel').id}" data-bs-slide-to="${index}"></li>`;
+        }
+      });
+    });
+  
+    /**
+     * Init swiper sliders
+     */
+    function initSwiper() {
+      document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+        let config = JSON.parse(
+          swiperElement.querySelector(".swiper-config").innerHTML.trim()
+        );
+  
+        if (swiperElement.classList.contains("swiper-tab")) {
+          initSwiperWithCustomPagination(swiperElement, config);
+        } else {
+          new Swiper(swiperElement, config);
+        }
+      });
+    }
+  
+    window.addEventListener("load", initSwiper);
+  
+    /**
+     * Initiate glightbox
+     */
+    const glightbox = GLightbox({
+      selector: '.glightbox'
+    });
+  
+  })();
